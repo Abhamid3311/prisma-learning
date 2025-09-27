9d284291acd407102fde69c7278de04306dc6e39
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-09-27 15:32:52 +0600

    Module 2 done

--- src/logging.ts
+++ src/logging.ts
@@ -1,9 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log(e);
+});
 
 const logging = async () => {
+  const getUsers = await prisma.user.findMany({});
 
-}
+  //   console.log(getUsers);
+};
 
 logging();
