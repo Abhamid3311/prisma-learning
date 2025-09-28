6c75ed79f4ded8aaf1a15f8b3b6ba5ef62f5687c
Abhamid3311 <www.abdulhamid3311@gmail.com>
2025-09-28 11:22:44 +0600

    Aggregate function example & count

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,24 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
- 
+  // Aggregare Only Work on Number values like age,price, number etc
+  const ageAvg = await prisma.user.aggregate({
+    _avg: { age: true },
+  });
+
+  const ageSum = await prisma.user.aggregate({
+    _sum: { age: true },
+  });
+
+  // Find Count of age fileds
+  const ageCount = await prisma.user.aggregate({
+    _count: { age: true },
+  });
+
+  // Total Row
+  const countUser = await prisma.user.count();
+
+  console.log(countUser);
 };
 
 main();
